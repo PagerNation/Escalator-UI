6ca61f75bc3ca5905b30a8eaa740552b61301fe5
Kevin Moses <kwm4385@rit.edu>
2016-11-27 17:42:35 -0800

    I have no idea what I'm doing

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -3,12 +3,11 @@ export default (store) => ({
 
   getComponent (nextState, next) {
     require.ensure([
-      './containers/HomeContainer',
-      '../../modules/user'
+      './containers/HomeContainer'
     ], (require) => {
-      const Home = require('./containers/HomeContainer').default
+      const Home = require('./containers/HomeContainer').default;
 
       next(null, Home);
-    })
+    });
   }
 });
