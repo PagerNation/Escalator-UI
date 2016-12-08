46dc27eaf9e737aff82e6c40c4c920683fd1300e
Jarryd Lee <jarrydrlee@gmail.com>
2016-12-08 10:43:07 -0500

    Add caddyfile to dist (#5)

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   chmod 400 EscalatorUI.pem
   NODE_ENV=production npm run compile
+  mv Caddyfile dist/Caddyfile
   tar -czf package.tgz dist
   scp -i EscalatorUI.pem -o StrictHostKeyChecking=no package.tgz $DEPLOY_USER@$DEPLOY_HOST:~/
   ssh -i EscalatorUI.pem -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/deploy.sh
