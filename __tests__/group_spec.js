8999d553dc92c98dd6ec2ce39f781f9bae4f0f3c
Kevin Moses <kwm4385@rit.edu>
2017-03-23 14:27:14 -0400

    Add user fixture and update snapshot

--- __tests__/group_spec.js
+++ __tests__/group_spec.js
@@ -4,11 +4,12 @@ import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 
 import group from './fixtures/group_response.json';
+import user from './fixtures/user.json';
 
 describe('GroupView', () => {
   it('renders GroupView', () => {
     spyOn(GroupView.prototype, 'componentWillMount').and.returnValue(null);
-    const component = renderer.create(<GroupView group={group}/>);
+    const component = renderer.create(<GroupView group={group} user={user} />);
     const json = component.toJSON();
     expect(json).toMatchSnapshot();
   })
