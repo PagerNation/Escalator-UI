d105e1837516fe3dc975af89e59e69e8b45df556
Jarryd Lee <jarrydrlee@gmail.com>
2017-04-24 21:06:01 -0400

    Fix body for group tickets

--- src/routes/Group/components/GroupView.js
+++ src/routes/Group/components/GroupView.js
@@ -95,7 +95,7 @@ class GroupView extends React.Component {
 
   componentWillMount() {
     this.props.fetchGroup(this.props.params.groupId);
-    this.props.fetchGroupTickets({ groupName: this.props.params.groupId });
+    this.props.fetchGroupTickets({ groupNames: this.props.params.groupId });
   }
 
   active() {
