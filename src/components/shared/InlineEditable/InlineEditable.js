556a2dadc0ae4bf0128dbd873f22bee4b422d7a9
Jarryd Lee <jarrydrlee@gmail.com>
2017-04-29 15:38:52 -0400

    Add keypress handler for inlineeditable

--- src/components/shared/InlineEditable/InlineEditable.js
+++ src/components/shared/InlineEditable/InlineEditable.js
@@ -15,6 +15,7 @@ class InlineEditable extends React.Component {
     _.bindAll(this,
       'handleMouseEnter',
       'handleMouseLeave',
+      'handleKeyPress',
       'toggleEditor',
       'handleSave',
       'handleChange',
@@ -37,6 +38,12 @@ class InlineEditable extends React.Component {
     this.setState({
       isHovered: false
     });
+  }
+
+  handleKeyPress(event) {
+    if (event.key == 'Enter') {
+      this.handleSave();
+    }
   }
 
   toggleEditor() {
@@ -88,6 +95,7 @@ class InlineEditable extends React.Component {
           size="small"
           onChange={this.handleChange}
           onBlur={this.handleBlur}
+          onKeyPress={this.handleKeyPress}
           action={{color: 'green', icon: 'check', onClick: this.handleSave}}
           defaultValue={this.props.value}
         />
