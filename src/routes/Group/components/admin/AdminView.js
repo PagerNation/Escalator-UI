e36ea63ceae5d7522ed3aaa628fd07884a18d37a
paradime <brw4824@rit.edu>
2017-04-30 19:09:11 -0400

    ran lint and added prop types

--- src/routes/Group/components/admin/AdminView.js
+++ src/routes/Group/components/admin/AdminView.js
@@ -1,116 +1,138 @@
-import React from "react";
-import { Icon, Divider, Header, Grid, Segment, Button } from 'semantic-ui-react';
-import _ from 'lodash';
+import React from 'react'
+import { Icon, Divider, Header, Grid, Segment, Button } from 'semantic-ui-react'
+import _ from 'lodash'
+import PropTypes from 'prop-types'
 
 class AdminView extends React.Component {
-  constructor() {
-    super();
+  constructor () {
+    super()
     _.bindAll(this,
-      "handleProcessRequest",
-      "handleUpgradeUser");
+      'handleProcessRequest',
+      'handleUpgradeUser')
   }
 
-  handleProcessRequest(userId, approved) {
-    this.props.processRequest(this.props.group.name, userId, approved);
+  handleProcessRequest (userId, approved) {
+    this.props.processRequest(this.props.group.name, userId, approved)
   }
 
-  handleUpgradeUser(userId) {
-    this.props.upgradeUser(this.props.group.name, userId);
+  handleUpgradeUser (userId) {
+    this.props.upgradeUser(this.props.group.name, userId)
   }
 
-  handleDowngradeUser(userId) {
-    this.props.deleteAdmin(this.props.group.name, userId);
+  handleDowngradeUser (userId) {
+    this.props.deleteAdmin(this.props.group.name, userId)
   }
 
-  renderJoinRequests() {
+  renderJoinRequests () {
     if (this.props.group.joinRequests && this.props.group.joinRequests.length > 0) {
-      return (<Grid className="group-requests">
-          <Grid.Column mobile={16} computer={8}>
-            <Header as="h4">Pending membership requests:</Header>
-            <div>
-              {this.props.group.joinRequests.map((user, i) =>
-                <Segment key={i} raised>
-                  <Header as="h5">{user.name}</Header>
-                  <span>{user.email}</span>
-                  <Button style={{marginTop: "-20px"}} floated="right" color="green" onClick={() => this.handleProcessRequest(user._id, true)}>Approve</Button>
-                  <Button style={{marginTop: "-20px"}} className="section-btn" floated="right" color="red" onClick={() => this.handleProcessRequest(user._id, false)}>Deny</Button>
-                </Segment>
+      return (<Grid className='group-requests'>
+        <Grid.Column mobile={16} computer={8}>
+          <Header as='h4'>Pending membership requests:</Header>
+          <div>
+            {this.props.group.joinRequests.map((user, i) =>
+              <Segment key={i} raised>
+                <Header as='h5'>{user.name}</Header>
+                <span>{user.email}</span>
+                <Button
+                  style={{ marginTop: '-20px' }}
+                  floated='right'
+                  color='green'
+                  onClick={() => this.handleProcessRequest(user._id, true)}>
+                    Approve
+                  </Button>
+                <Button
+                  style={{ marginTop: '-20px' }}
+                  className='section-btn'
+                  floated='right'
+                  color='red'
+                  onClick={() => this.handleProcessRequest(user._id, false)}>
+                    Deny
+                  </Button>
+              </Segment>
               )}
-            </div>
-          </Grid.Column>
-        </Grid>);
+          </div>
+        </Grid.Column>
+      </Grid>)
     } else {
-      return (<b>No pending join requests</b>);
+      return (<b>No pending join requests</b>)
     }
   }
 
-  userUpgradeButtons(upgrade) {
+  userUpgradeButtons (upgrade) {
     const display = {
       'upgrade': {
         icon: 'arrow up',
         color: 'green',
-        compare(user, admins){ return admins.indexOf(user._id) === -1; },
-        changefunc(comp, user){ return comp.handleUpgradeUser(user._id); }
+        compare (user, admins) { return admins.indexOf(user._id) === -1 },
+        changefunc (comp, user) { return comp.handleUpgradeUser(user._id) }
       },
       'downgrade': {
         icon: 'arrow down',
         color: 'red',
-        compare(user, admins){ return admins.indexOf(user._id) !== -1 },
-        changefunc(comp, user){ return comp.handleDowngradeUser(user._id); }
-      }    
+        compare (user, admins) { return admins.indexOf(user._id) !== -1 },
+        changefunc (comp, user) { return comp.handleDowngradeUser(user._id) }
+      }
     }
-    return [,...this.props.group.users].filter((user) => 
+    return [...this.props.group.users].filter((user) =>
         display[upgrade].compare(user, this.props.group.admins, this.props.user._id)
     ).map((user, i) =>
       <Segment key={i} raised>
-        <span as="h5">{user.name}</span>
-        <Button icon 
-          size="mini" 
-          style={{marginTop: "-5px"}} 
-          floated="right" 
-          color={display[upgrade].color} 
+        <span as='h5'>{user.name}</span>
+        <Button icon
+          size='mini'
+          style={{ marginTop: '-5px' }}
+          floated='right'
+          color={display[upgrade].color}
           disabled={user._id === this.props.user._id}
           onClick={() => display[upgrade].changefunc(this, user)}>
-            <Icon name={display[upgrade].icon}/>
+          <Icon name={display[upgrade].icon} />
         </Button>
       </Segment>
     )
   }
 
-
-  renderUpgradeTable() {
+  renderUpgradeTable () {
     return (<Grid columns={2} divided className='user-upgrade'>
       <Grid.Column mobile={16} computer={8}>
-        <Header as="h4">Members:</Header>
-          <div>
-            <Segment.Group>
-              {this.userUpgradeButtons('upgrade')}
-            </Segment.Group>
-          </div>
+        <Header as='h4'>Members:</Header>
+        <div>
+          <Segment.Group>
+            {this.userUpgradeButtons('upgrade')}
+          </Segment.Group>
+        </div>
       </Grid.Column>
       <Grid.Column mobile={16} computer={8}>
-        <Header as="h4">Admins:</Header>
-            <Segment.Group>
-              {this.userUpgradeButtons('downgrade')}
-            </Segment.Group>
+        <Header as='h4'>Admins:</Header>
+        <Segment.Group>
+          {this.userUpgradeButtons('downgrade')}
+        </Segment.Group>
       </Grid.Column>
-    </Grid>);
+    </Grid>)
   }
 
-  render() {
-    if (this.props.group.admins.includes(this.props.user._id)){
+  render () {
+    if (this.props.group.admins.includes(this.props.user._id)) {
       return (<div>
-        <Divider/>
-        <Header as="h3">Administration</Header>
+        <Divider />
+        <Header as='h3'>Administration</Header>
         {this.renderJoinRequests()}
-        <Divider hidden/>
+        <Divider hidden />
         {this.renderUpgradeTable()}
-        <Divider/>
-      </div>);
+        <Divider />
+      </div>)
     } else {
-      return null;
+      return null
     }
   }
+
 }
 
-export default AdminView;
+AdminView.propTypes = {
+  processRequest: PropTypes.func,
+  group: PropTypes.object,
+  user: PropTypes.object,
+  upgradeUser: PropTypes.func,
+  deleteAdmin: PropTypes.func
+}
+
+export default AdminView
