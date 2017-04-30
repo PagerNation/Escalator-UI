07090a0631180b1dded5a8146eff67bb749695eb
paradime <brw4824@rit.edu>
2017-04-30 18:09:26 -0400

    final iteration of ui

--- src/routes/Group/components/admin/AdminView.js
+++ src/routes/Group/components/admin/AdminView.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Divider, Header, Grid, Segment, Button } from 'semantic-ui-react';
+import { Icon, Divider, Header, Grid, Segment, Button } from 'semantic-ui-react';
 import _ from 'lodash';
 
 class AdminView extends React.Component {
@@ -35,46 +35,58 @@ class AdminView extends React.Component {
     }
   }
 
-  userDowngradeButtons() {
-    return [,...this.props.group.users].filter((user) =>
-        this.props.group.admins.indexOf(user._id) !== -1
+  userUpgradeButtons(upgrade) {
+    const display = {
+      'upgrade': {
+        icon: 'arrow up',
+        color: 'green',
+        compare(user, admins){ return admins.indexOf(user._id) === -1 }
+      },
+      'downgrade': {
+        icon: 'arrow down',
+        color: 'red',
+        compare(user, admins){ return admins.indexOf(user._id) !== -1 }
+      }    
+    }
+    return [,...this.props.group.users].filter((user) => 
+        display[upgrade].compare(user, this.props.group.admins, this.props.user._id)
     ).map((user, i) =>
       <Segment key={i} raised>
-        <Header as="h5">{user.name}</Header>
-        <span>{user.name}</span>
-        <Button style={{marginTop: "-20px"}} floated="right" color="red" onClick={() => this.handleProcessRequest(user._id, true)}>Downgrade to Normal User</Button>
-      </Segment>
-    )
-  }
-  userUpgradeButtons() {
-    return [,...this.props.group.users].filter((user) =>
-        this.props.group.admins.indexOf(user._id) === -1
-    ).map((user, i) =>
-      <Segment key={i} raised>
-        <Header as="h5">{user.name}</Header>
-        <span>{user.name}</span>
-        <Button style={{marginTop: "-20px"}} floated="right" color="green" onClick={() => this.handleProcessRequest(user._id, true)}>Upgrade to Admin</Button>
+        <span as="h5">{user.name}</span>
+        <Button icon 
+          size="mini" 
+          style={{marginTop: "-5px"}} 
+          floated="right" 
+          color={display[upgrade].color} 
+          disabled={user._id === this.props.user._id}
+          onClick={() => this.handleProcessRequest(user._id, true)}>
+            <Icon name={display[upgrade].icon}/>
+        </Button>
       </Segment>
     )
   }
 
+
   renderUpgradeTable() {
-    return (<Grid className='user-upgrade'>
+    return (<Grid columns={2} divided className='user-upgrade'>
       <Grid.Column mobile={16} computer={8}>
-        <Header as="h4">Upgrade users to admin:</Header>
+        <Header as="h4">Members:</Header>
           <div>
-            {this.userUpgradeButtons()}
+            <Segment.Group>
+              {this.userUpgradeButtons('upgrade')}
+            </Segment.Group>
           </div>
-        <Header as="h4">Downgrade admins to regular user:</Header>
-          <div>
-            {this.userDowngradeButtons()}
-          </div>
+      </Grid.Column>
+      <Grid.Column mobile={16} computer={8}>
+        <Header as="h4">Admins:</Header>
+            <Segment.Group>
+              {this.userUpgradeButtons('downgrade')}
+            </Segment.Group>
       </Grid.Column>
     </Grid>);
   }
 
   render() {
-    console.log(this.props.group)
     if (this.props.group.admins.includes(this.props.user._id)){
       return (<div>
         <Divider/>
